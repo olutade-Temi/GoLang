f4c32442345700772da27d52c70e061dacd038c2
unknown <topeolutade25@gmail.comgit config --global user.name>
2021-12-24 15:09:44 +0100

    first commit

---
+++ hello_world.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World, this is my first interaction with GO Programming Language")
+	foo()
+
+}
+func foo() {
+	fmt.Println("This is just another line")
+}
