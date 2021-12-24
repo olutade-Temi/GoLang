05384f51de0ebd4f1ec26119bc80d7b13656fc7b
unknown <topeolutade25@gmail.comgit config --global user.name>
2021-12-24 15:21:06 +0100

    second commit

--- hello_world.go
+++ hello_world.go
@@ -6,7 +6,15 @@ func main() {
 	fmt.Println("Hello World, this is my first interaction with GO Programming Language")
 	foo()
 
+	for i := 0
+		i < 100;
+	i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
+
 func foo() {
 	fmt.Println("This is just another line")
 }
