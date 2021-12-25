e7cb1df38ef7f0d9f686fea8f0366e676ec11445
unknown <topeolutade25@gmail.comgit config --global user.name>
2021-12-25 21:49:39 +0100

    updated the creating_type file

--- creating_type.go
+++ creating_type.go
@@ -5,10 +5,14 @@ import "fmt"
 type temitope int
 
 var x temitope
+var y int
 
 func main() {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
 	fmt.Println(x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T", y)
 }
