2d1d1c5efaebc6ed8185a02c7dedbcaad832eaaa
unknown <topeolutade25@gmail.comgit config --global user.name>
2021-12-25 21:41:17 +0100

    Added a new file on creating types

--- Exercises.go
+++ Exercises.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
+var a int = 42
+var b string = "James Bond"
+var c bool = true
+
 func main() {
 	x := 42
 	y := "James Bond"
 	z := true
+	fmt.Println(x, y, z)
 	fmt.Println(x)
 	fmt.Println(y)
-	fmt.Print(z)
-
+	fmt.Println(z)
+	s := fmt.Sprintf("%v\t%v\t%v\t", a, b, c)
+	fmt.Println(s)
 }
