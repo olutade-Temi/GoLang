36c254d88ff4ba12633cb5e9f3d0900214b1f2e3
unknown <topeolutade25@gmail.comgit config --global user.name>
2021-12-29 15:18:23 +0100

    file updated

--- stringtobyte.go
+++ stringtobyte.go
@@ -7,8 +7,19 @@ func main() {
 	fmt.Println(s)
 	fmt.Printf("%T\n", s)
 
-	sb := []byte(s)
-	fmt.Println(sb)
-	fmt.Printf("%T\n", sb)
+	bs := []byte(s)
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
 
+	c := "H"
+	fmt.Println(c)
+	
+	cs := []byte(c)
+	fmt.Println(cs)
+
+	n := bs[0]
+	fmt.Println(n)
+	fmt.Printf("%T\n", n)
+	fmt.Printf("%b\n", n)
+	fmt.Printf("%#X\n", n)
 }
